c92873ccaf2bdb7f4e9092a2c4cc20a075d28526
alleksandrb <100118742+alleksandrb@users.noreply.github.com>
2022-02-26 08:41:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 "use strict"
 
-// -----------------buttons
-const anchors = document.querySelectorAll('a[href^="#"]')
-for (let anchor of anchors) {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault()
-        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-
-        document.querySelector(goto).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        })
-    })
-}
 
 // -----------header
 let lastScrollTop = 0;
